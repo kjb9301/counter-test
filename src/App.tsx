97de882799dd83b1d1a84f5e43d0879e22367702
jbkang <kjb9301@naver.com>
2020-09-09 16:37:51 +0900

    style: Add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import GlobalStyle from './lib/styles/globalStyles';
 import * as Pages from './pages';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Pages.Main} />
-        <Route path='/basket' component={Pages.Basket} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Pages.Main} />
+          <Route path='/basket' component={Pages.Basket} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/lib/styles/globalStyles.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body{
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    background-color: #ffffff;
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  input, button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  h1, h2, h3, h4, h5, h6{
+    font-family:'Maven Pro', sans-serif;
+  }
+
+  ol, ul, li {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
