ac007c28c041840bcca9c70390e11f06ec598561
jbkang <kjb9301@naver.com>
2020-09-23 18:59:42 +0900

    feat: Create store

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store/store';
 import GlobalStyle from './lib/styles/globalStyles';
 import * as Pages from './pages';
 
@@ -8,12 +10,14 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Pages.Main} />
-          <Route path='/basket' component={Pages.Basket} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Pages.Main} />
+            <Route path='/basket' component={Pages.Basket} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
